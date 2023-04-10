7579937239d09961e0fef81c02df25d2ebebc865
faria-karim-porna <faria.porna.08@gmail.com>
2023-04-10 15:26:16 +0000

    Convert App, App.test And Index To Tsx And Added Tsconfig

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, { createContext, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NotFound from "./Components/NotFound/NotFound";
+import Review from "./Components/Review/Review";
+import ServiceList from "./Components/ServiceList/ServiceList";
+import Home from "./Components/Home/Home";
+import AddService from "./Components/AddService/AddService";
+import MakeAdmin from "./Components/MakeAdmin/MakeAdmin";
+import Login from "./Components/Login/Login";
+import MainDashboard from "./Components/MainDashboard/MainDashboard";
+import Order from "./Components/Order/Order";
+import AdminDataList from "./Components/AdminDataList/AdminDataList";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+
+export const UserContext = createContext();
+export function App(props: any): JSX.Element {
+  const [loggedInUser, setLoggedInUser] = useState();
+  return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <PrivateRoute path="/dashboard/:serviceTitle">
+          <MainDashboard></MainDashboard>
+        </PrivateRoute>
+        <Route path="/customer/order">
+          <Order></Order>
+        </Route>
+        <Route path="/customer/serviceList">
+          <ServiceList></ServiceList>
+        </Route>
+        <Route path="/customer/review">
+          <Review></Review>
+        </Route>
+        <Route path="/admin/serviceList">
+          <AdminDataList></AdminDataList>
+        </Route>
+        <Route path="/admin/addService">
+          <AddService></AddService>
+        </Route>
+        <Route path="/admin/makeAdmin">
+          <MakeAdmin></MakeAdmin>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
