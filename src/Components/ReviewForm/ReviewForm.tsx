6f6cfb13a58475ce99a1b0ad44bc23dcdbb5cd4e
faria-karim-porna <faria.porna.08@gmail.com>
2023-04-15 20:19:54 +0000

    Fixed Review Form Component

--- src/Components/ReviewForm/ReviewForm.tsx
+++ src/Components/ReviewForm/ReviewForm.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { FocusEvent, useState } from "react";
 import "./ReviewForm.css";
 const ReviewForm = () => {
   const [name, setName] = useState("");
   const [designation, setDesignation] = useState("");
   const [description, setDescription] = useState("");
+  const [message, setMessage] = useState("");
   const img = localStorage.getItem("photo");
-  const handleBlur = (e: any) => {
+  const handleBlur = (e: FocusEvent<HTMLTextAreaElement> | FocusEvent<HTMLInputElement>) => {
     if (e.target.name == "name") {
       setName(e.target.value);
     }
@@ -28,37 +29,43 @@ const ReviewForm = () => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newReview),
-    }).then((res) => res.json());
-    e.preventdefault();
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      });
   };
   return (
     <div className="row">
       <div className="col-md-9">
-        <form onSubmit={handleSubmit}>
-          <div className="row form-class">
-            <div className="col-md-12">
-              <input type="text" placeholder="Your name" name="name" className="w-100" onBlur={handleBlur} required></input>
-            </div>
-            <div className="col-md-12">
-              <input
-                type="text"
-                placeholder="Company's name Designation"
-                name="designation"
-                className="w-100"
-                onBlur={handleBlur}
-                required
-              ></input>
-            </div>
-            <div className="col-md-12">
-              <textarea className="textArea w-100" name="description" placeholder="Description" onBlur={handleBlur} required></textarea>
-            </div>
-            <div className="col-md-4">
-              <button className="w-100 dark-button-class" type="submit">
-                Send
-              </button>
-            </div>
+        <div className="row form-class">
+          <div className="col-md-12">
+            <input type="text" placeholder="Your name" name="name" className="w-100" onBlur={handleBlur} required></input>
           </div>
-        </form>
+          <div className="col-md-12">
+            <input
+              type="text"
+              placeholder="Company's name Designation"
+              name="designation"
+              className="w-100"
+              onBlur={handleBlur}
+              required
+            ></input>
+          </div>
+          <div className="col-md-12">
+            <textarea className="textArea w-100" name="description" placeholder="Description" onBlur={handleBlur} required></textarea>
+          </div>
+          <div className="col-md-4">
+            <button className="w-100 dark-button-class" onClick={handleSubmit}>
+              Send
+            </button>
+          </div>
+        </div>
+        {message ? (
+          <div className="d-flex justify-content-center">
+            <div className="text-success font-weight-bold">{message}</div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
