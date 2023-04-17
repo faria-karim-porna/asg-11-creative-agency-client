566fd1b80deb04a3c0c11fefb02ca3a83c4923d0
faria-karim-porna <faria.porna.08@gmail.com>
2023-04-17 19:57:02 +0000

    Added Dashboard View And Enum

---
+++ src/Components/DashboardView/DashboardView.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import SideNav from "../SideNav/SideNav";
+import { useAppSelector } from "../../core/redux/reduxStore";
+import { shallowEqual } from "react-redux";
+import { EnumView } from "../../core/enums/EnumView";
+import MakeAdmin from "../MakeAdmin/MakeAdmin";
+import ServiceList from "../ServiceList/ServiceList";
+import AddService from "../AddService/AddService";
+import MainDashboard from "../MainDashboard/MainDashboard";
+import AdminDataList from "../AdminDataList/AdminDataList";
+import Order from "../Order/Order";
+import Review from "../Review/Review";
+
+export const DashboardView = () => {
+  const store = useAppSelector(
+    (state) => ({
+      view: state.UI.view,
+    }),
+    shallowEqual
+  );
+  return (
+    <>
+      <SideNav />
+      {store.view === EnumView.ViewService ? (
+        <AdminDataList />
+      ) : store.view === EnumView.AddService ? (
+        <AddService />
+      ) : store.view === EnumView.AddAdmin ? (
+        <MakeAdmin />
+      ) : store.view === EnumView.AddOrder ? (
+        <Order />
+      ) : store.view === EnumView.PersonalService ? (
+        <ServiceList />
+      ) : store.view === EnumView.AddReview ? (
+        <Review />
+      ) : null}
+    </>
+  );
+};
---
+++ src/core/enums/EnumView.ts
@@ -0,0 +1,9 @@
+export enum EnumView {
+  None = "None",
+  AddOrder = "AddOrder",
+  PersonalService = "PersonalService",
+  AddReview = "AddReview",
+  ViewService = "ViewService",
+  AddService = "AddService",
+  AddAdmin = "AddAdmin",
+}
